bba5b395e2384fc6aebbade122491cfbc7dee43c
lucia blazova <blazova.lucia@gmail.com>
2022-01-15 13:30:08 +0000

    deploy fct_orders

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,11 @@
+ with orders as (
+    select
+        order_id,
+        customer_id,
+        amount
+    from {{ ref('stg_orders') }}
+    join {{ ref('stg_payments')}}
+    using(order_id)
+ )
+
+ select * from orders
