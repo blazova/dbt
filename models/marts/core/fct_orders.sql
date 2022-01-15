f0918e6f000a2781ab887703673b9fe3d45399cc
lucia blazova <blazova.lucia@gmail.com>
2022-01-15 13:54:25 +0000

    fix data discrepancies

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,11 +1,30 @@
- with orders as (
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
+
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
     select
         order_id,
-        customer_id,
-        amount
-    from {{ ref('stg_orders') }}
-    join {{ ref('stg_payments')}}
-    using(order_id)
- )
+        sum(case when status = 'success' then amount end) as amount
 
- select * from orders
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
