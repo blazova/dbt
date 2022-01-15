efcb58ec840c1dfe6885b0d583b676f43d14215d
lucia blazova <blazova.lucia@gmail.com>
2022-01-15 14:26:10 +0000

    deploy src_jaffle_shop.yml and refactor usage of the source

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -5,7 +5,7 @@ with customers as (
         first_name,
         last_name
 
-    from d4t93bhmgqavk5.jaffle_shop.customers
+    from {{ source('jaffle_shop', 'customers') }}
 )
 
 select * from customers
